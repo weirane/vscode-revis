84026d561ce9e0d8df326ef88ca7d7af49442fac
Wang Ruochen <wrc@ruo-chen.wang>
2023-02-22 10:08:10 -0800

    Extract

--- src/extension.ts
+++ src/extension.ts
@@ -64,46 +64,25 @@ function decorateImage() {
   const afterLineText = vscode.window.createTextEditorDecorationType({
     isWholeLine: true,
   });
-  const i502 = image502(
-    60,
-    5,
-    7,
-    6,
-    "`a` borrowed immutably in this region",
-    "`a` borrowed mutably, conflicting with the immutable borrow region",
-    "tip: move the mutable borrow out of the immutable borrow area"
-  );
-  const i503 = image503(
-    "`value` borrowed mutably in this region",
-    "`value` used here, conflicting with the borrow region",
-    "tip: move the use of `value` out of the borrow region"
-  );
-  const i505 = image505(
-    800,
-    4 * G.lineheight,
-    "`x` borrowed in this region",
-    "`x` moved into function `eat()`",
-    "tip: the move of a value should happen when it is not borrowed\nafter the move, the value can no longer be borrowed"
-  );
   const i597 = image597();
 
   editor.setDecorations(afterLineText, [
-    image2decoration(i502, 4),
-    image2decoration(i503, 12),
-    image2decoration(i505, 24),
-    image2decoration(i597, 61),
+    image502(60, 5, 8, 6, "a"),
+    image503(0, 14, 16, 15, "value"),
+    image505(0, 26, 28, 27, "x", "the function"),
+    image2decoration(i597, 62),
   ]);
 }
 
-function image502(
+function regionPointConflict(
   xshift: number,
   fromline: number,
   toline: number,
   errorline: number,
-  imm: string,
-  mut: string,
+  regiontext: string,
+  pointtext: string,
   tip: string
-): Svg {
+) {
   const svgimg = newSvg(800, G.lineheight * (toline - fromline + 2));
   const canvas = svgimg.group().attr({
     fill: "transparent",
@@ -111,59 +90,58 @@ function image502(
     style: `font-family: monospace; font-size: ${G.fontsize}px; overflow: visible;`,
   });
   canvas.path(`M0,0 L10,0 l0,${G.lineheight * (toline - fromline + 1)} l-10,0`).stroke("cyan");
-  canvas.plain(imm).fill("cyan").attr({ x: 20, y: G.fontsize });
-  canvas.path(`M0,${1.5 * G.lineheight} l20,0`).stroke("red");
+  canvas.plain(regiontext).fill("cyan").attr({ x: 20, y: G.fontsize });
+  canvas.path(`M0,${(errorline - fromline + 0.5) * G.lineheight} l20,0`).stroke("red");
   canvas
-    .plain(mut)
+    .plain(pointtext)
     .fill("red")
     .attr({ x: 30, y: G.fontsize + G.lineheight * (errorline - fromline) });
   canvas
-    .plain(tip)
-    .fill("white")
-    .attr({ x: 20, y: G.fontsize + G.lineheight * (toline - fromline) });
-  return svgimg;
-}
-
-function image503(mut: string, use: string, tip: string): Svg {
-  const svgimg = newSvg(800, 3 * G.lineheight);
-  const canvas = svgimg.group().attr({
-    fill: "transparent",
-    transform: "translate(0, 0)",
-    style: `font-family: monospace; font-size: ${G.fontsize}px; overflow: visible;`,
-  });
-  canvas.path(`M0,0 L10,0 l0,${G.lineheight * 3} l-10,0`).stroke("cyan");
-  canvas.plain(mut).fill("cyan").attr({ x: 20, y: G.fontsize });
-  canvas.path(`M0,${1.5 * G.lineheight} l20,0`).stroke("red");
-  canvas
-    .plain(use)
-    .fill("red")
-    .attr({ x: 30, y: G.fontsize + G.lineheight });
-  canvas
-    .plain(tip)
-    .fill("white")
-    .attr({ x: 20, y: G.fontsize + 2 * G.lineheight });
-  return svgimg;
-}
-
-function image505(width: number, height: number, borrow: string, move: string, tip: string): Svg {
-  const svgimg = newSvg(width, height);
-  const canvas = svgimg.group().attr({
-    fill: "transparent",
-    transform: "translate(0, 0)",
-    style: `font-family: monospace; font-size: ${G.fontsize}px; overflow: visible;`,
-  });
-  canvas.path(`M0,0 L10,0 l0,${G.lineheight * 3} l-10,0`).stroke("cyan");
-  canvas.plain(borrow).fill("cyan").attr({ x: 20, y: G.fontsize });
-  canvas.path(`M0,${1.5 * G.lineheight} l20,0`).stroke("red");
-  canvas
-    .plain(move)
-    .fill("red")
-    .attr({ x: 30, y: G.fontsize + G.lineheight });
-  canvas
     .text(tip)
     .fill("white")
-    .attr({ x: 20, y: G.fontsize + 2 * G.lineheight });
-  return svgimg;
+    .attr({ x: 20, y: G.fontsize + G.lineheight * (toline - fromline) });
+  return image2decoration(svgimg, fromline - 1);
+}
+
+function image502(
+  xshift: number,
+  fromline: number,
+  toline: number,
+  errorline: number,
+  borrowed: string
+): vscode.DecorationOptions {
+  const imm = `\`${borrowed}\` borrowed immutably in this region`;
+  const mut = `\`${borrowed}\` borrowed mutably here, conflicting with the immutable borrow`;
+  const tip = "tip: move the mutable borrow out of the immutable borrow area";
+  return regionPointConflict(xshift, fromline, toline, errorline, imm, mut, tip);
+}
+
+function image503(
+  xshift: number,
+  fromline: number,
+  toline: number,
+  errorline: number,
+  borrowed: string
+): vscode.DecorationOptions {
+  const imm = `\`${borrowed}\` borrowed mutably in this region`;
+  const mut = `\`${borrowed}\` used here, conflicting with the borrow`;
+  const tip = `tip: move the use of \`${borrowed}\` out of the borrow region`;
+  return regionPointConflict(xshift, fromline, toline, errorline, imm, mut, tip);
+}
+
+function image505(
+  xshift: number,
+  fromline: number,
+  toline: number,
+  errorline: number,
+  borrowed: string,
+  movein: string
+): vscode.DecorationOptions {
+  const imm = `\`${borrowed}\` borrowed in this region`;
+  const mut = `\`${borrowed}\` moved into ${movein}`;
+  const tip =
+    "tip: the move of a value should happen when it is not borrowed\nafter the move, the value can no longer be borrowed";
+  return regionPointConflict(xshift, fromline, toline, errorline, imm, mut, tip);
 }
 
 function image597(): Svg {
@@ -183,149 +161,6 @@ function image597(): Svg {
     .fill("red")
     .attr({ x: 0, y: G.fontsize + 3 * G.lineheight });
   return svgimg;
-}
-
-function decorate502() {
-  const editor = vscode.window.activeTextEditor;
-  if (!editor) {
-    console.log("no editor");
-    return;
-  }
-  const afterLineText = vscode.window.createTextEditorDecorationType({
-    isWholeLine: true,
-    after: { margin: `0 0 0 2em` },
-  });
-  const [info1l, info1f, info1t, info1m] = [3, 12, 14, "immutable borrow occurs here"];
-  const [info2l, info2f, info2t, info2m] = [5, 19, 20, "immutable borrow later used here"];
-  const [errorl, errorf, errort, errorm] = [4, 8, 9, "mutable borrow occurs here"];
-  const message = "cannot borrow `*a` as mutable because it is also borrowed as immutable";
-  editor.setDecorations(afterLineText, [
-    {
-      range: new vscode.Range(info1l, 0, info1l, 0),
-      renderOptions: {
-        after: {
-          color: "cyan",
-          contentText: info1m,
-        },
-      },
-    },
-    {
-      range: new vscode.Range(info2l, 0, info2l, 0),
-      renderOptions: {
-        after: {
-          color: "cyan",
-          contentText: info2m,
-        },
-      },
-    },
-    {
-      range: new vscode.Range(errorl, 0, errorl, 0),
-      renderOptions: {
-        after: {
-          color: "red",
-          contentText: errorm,
-        },
-      },
-    },
-  ]);
-  const relatedInfo = vscode.window.createTextEditorDecorationType({
-    borderWidth: "1px",
-    borderStyle: "solid",
-    borderColor: "cyan",
-  });
-  editor.setDecorations(relatedInfo, [
-    {
-      range: new vscode.Range(info1l, info1f, info1l, info1t),
-    },
-    {
-      range: new vscode.Range(info2l, info2f, info2l, info2t),
-    },
-  ]);
-
-  const errorInfo = vscode.window.createTextEditorDecorationType({
-    borderWidth: "1px",
-    borderStyle: "solid",
-    borderColor: "red",
-  });
-  editor.setDecorations(errorInfo, [
-    {
-      range: new vscode.Range(errorl, errorf, errorl, errort),
-      hoverMessage: message,
-    },
-  ]);
-}
-
-function decorate505() {
-  const editor = vscode.window.activeTextEditor;
-  if (!editor) {
-    console.log("no editor");
-    return;
-  }
-  const afterLineText = vscode.window.createTextEditorDecorationType({
-    isWholeLine: true,
-    // cursor: 'crosshair',
-    after: {
-      margin: `0 0 0 2em`,
-    },
-  });
-  const [info1l, info1f, info1t, info1m] = [16, 30, 32, "borrow of `x` occurs here"];
-  const [info2l, info2f, info2t, info2m] = [18, 11, 22, "borrow later used here"];
-  const [errorl, errorf, errort, errorm] = [17, 8, 9, "move out of `x` occurs here"];
-  const message = "cannot move out of `x` because it is borrowed";
-  editor.setDecorations(afterLineText, [
-    {
-      range: new vscode.Range(info1l, 0, info1l, 0),
-      renderOptions: {
-        after: {
-          color: "cyan",
-          contentText: info1m,
-        },
-      },
-    },
-    {
-      range: new vscode.Range(info2l, 0, info2l, 0),
-      renderOptions: {
-        after: {
-          color: "cyan",
-          contentText: info2m,
-        },
-      },
-    },
-    {
-      range: new vscode.Range(errorl, 0, errorl, 0),
-      renderOptions: {
-        after: {
-          color: "red",
-          contentText: errorm,
-        },
-      },
-    },
-  ]);
-  const relatedInfo = vscode.window.createTextEditorDecorationType({
-    borderWidth: "1px",
-    borderStyle: "solid",
-    borderColor: "cyan",
-  });
-  editor.setDecorations(relatedInfo, [
-    {
-      range: new vscode.Range(info1l, info1f, info1l, info1t),
-    },
-    {
-      range: new vscode.Range(info2l, info2f, info2l, info2t),
-    },
-  ]);
-
-  const errorInfo = vscode.window.createTextEditorDecorationType({
-    borderWidth: "1px",
-    borderStyle: "solid",
-    borderColor: "red",
-  });
-  editor.setDecorations(errorInfo, [
-    {
-      range: new vscode.Range(errorl, errorf, errorl, errort),
-      hoverMessage: message,
-    },
-  ]);
 }
 
 // This method is called when your extension is deactivated
