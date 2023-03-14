a6b200d907a97de1cfaf19a166a6e3f4cb27c7fa
Wang Ruochen <wrc@ruo-chen.wang>
2023-03-14 13:44:38 -0700

    Add 506 and 597

--- src/errorviz.ts
+++ src/errorviz.ts
@@ -20,7 +20,13 @@ function range(from: number, to: number): ReadonlyArray<number> {
  */
 function getXshift(editor: vscode.TextEditor, de: number, au: number): number {
   const longest = Math.max(
-    ...range(de, au + 1).map((li) => editor.document.lineAt(li).text.length)
+    ...range(de, au + 1).map((li) => {
+      try {
+        return editor.document.lineAt(li).text.length;
+      } catch (_) {
+        return 0;
+      }
+    })
   );
   const first = editor.document.lineAt(de).text.length;
   return longest - first;
@@ -139,7 +145,6 @@ export const G = {
       diaginfo.dectype = newDecorationType();
     }
     diaginfo.displayed = true;
-    log.info(diaginfo.svg.renderOptions?.after?.contentIconPath?.toString());
     editor.setDecorations(diaginfo.dectype, [diaginfo.svg]);
   },
 };
@@ -160,6 +165,10 @@ function imageByCode(
       return image503(editor, diag);
     case "E0505":
       return image505(editor, diag);
+    case "E0506":
+      return image506(editor, diag);
+    case "E0597":
+      return image597(editor, diag);
     default:
       log.info(`unsupported error code ${diag.code!.value}`);
       return `unsupported error code ${diag.code!.value}`;
@@ -172,11 +181,12 @@ function regionPointConflict(
   fromline: number,
   toline: number,
   errorline: number,
+  tipline: number,
   regiontext: string,
   pointtext: string,
   tip: string
 ) {
-  const svgimg = newSvg(800, G.lineheight * (toline - fromline + 2));
+  const svgimg = newSvg(800 + xshift, G.lineheight * (Math.max(toline, tipline) - fromline + 2));
   const canvas = svgimg.group().attr({
     fill: "transparent",
     transform: `translate(${xshift}, 0)`,
@@ -192,7 +202,7 @@ function regionPointConflict(
   canvas
     .text(tip)
     .fill("white")
-    .attr({ x: 20, y: G.fontsize + G.lineheight * (toline - fromline) });
+    .attr({ x: 20, y: G.fontsize + G.lineheight * (tipline - fromline) });
   return image2decoration(svgimg, fromline);
 }
 
@@ -215,7 +225,7 @@ export function image502(
   const imm = `\`${borrowed}\` borrowed immutably in this region`;
   const mut = `\`${borrowed}\` borrowed mutably here, conflicting with the immutable borrow`;
   const tip = "tip: move the mutable borrow out of the immutable borrow area";
-  return regionPointConflict(xshift, fromline, toline, errorline, imm, mut, tip);
+  return regionPointConflict(xshift, fromline, toline, errorline, toline, imm, mut, tip);
 }
 
 export function image503(
@@ -236,7 +246,7 @@ export function image503(
   const imm = `\`${borrowed}\` borrowed mutably in this region`;
   const mut = `\`${borrowed}\` used here, conflicting with the borrow`;
   const tip = `tip: move the use of \`${borrowed}\` out of the borrow region`;
-  return regionPointConflict(xshift, fromline, toline, errorline, imm, mut, tip);
+  return regionPointConflict(xshift, fromline, toline, errorline, toline, imm, mut, tip);
 }
 
 export function image505(
@@ -260,24 +270,76 @@ export function image505(
   const mut = `\`${borrowed}\` moved into ${movein}`;
   const tip =
     "tip: the move of a value should happen when it is not borrowed.\nafter the move, the value can no longer be borrowed";
-  return regionPointConflict(xshift, fromline, toline, errorline, imm, mut, tip);
+  return regionPointConflict(xshift, fromline, toline, errorline, toline, imm, mut, tip);
 }
 
-function image597(): Svg {
-  const svgimg = newSvg(800, 7 * G.lineheight);
-  const canvas = svgimg.group().attr({
-    fill: "transparent",
-    transform: "translate(80, 0)",
-    style: `font-family: monospace; font-size: ${G.fontsize}px; overflow: visible;`,
-  });
-  canvas.path(`M0,0 L10,0 l0,${G.lineheight * 3} l-10,0`).stroke("cyan");
-  canvas
-    .plain("`y`'s lifetime")
-    .fill("cyan")
-    .attr({ x: 20, y: 1.5 * G.lineheight });
-  canvas
-    .plain("`x.x` still refers to `y`, but `y` doesn't exist anymore")
-    .fill("red")
-    .attr({ x: 0, y: G.fontsize + 3 * G.lineheight });
-  return svgimg;
+export function image506(
+  editor: vscode.TextEditor,
+  diag: vscode.Diagnostic
+): vscode.DecorationOptions | string {
+  const borrowed = /^cannot assign to `(.+)` because it is borrowed/.exec(diag.message)![1];
+  const errorline = diag.range.start.line;
+  const fromline = diag.relatedInformation?.filter((d) => d.message.endsWith("occurs here"))[0]
+    ?.location.range.start.line;
+  const toline = diag.relatedInformation?.filter((d) =>
+    d.message.endsWith("borrow later used here")
+  )[0]?.location.range.end.line;
+  if (borrowed === undefined || fromline === undefined || toline === undefined) {
+    return "cannot parse diagnostics";
+  }
+  // TODO: parse movein
+  const movein = "something";
+  const xshift = getXshift(editor, fromline, toline) * G.charwidth;
+  const mut = `\`${borrowed}\` moved into ${movein}`;
+  const tip = "tip: when a variable is borrowed by another variable, it cannot be reassigned";
+  return regionPointConflict(
+    xshift,
+    fromline,
+    toline,
+    errorline,
+    toline,
+    `\`${borrowed}\` borrowed in this region`,
+    `\`${borrowed}\` assigned to another value`,
+    tip
+  );
+}
+
+function image597(
+  editor: vscode.TextEditor,
+  diag: vscode.Diagnostic
+): vscode.DecorationOptions | string {
+  const borrowed = /`(.+)` does not live long enough/.exec(diag.message)![1];
+  const validfrom = diag.range.start.line;
+  const validto = diag.relatedInformation?.filter((d) =>
+    d.message.endsWith("dropped here while still borrowed")
+  )[0]?.location.range.end.line;
+  const lateruseObj = diag.relatedInformation?.filter((d) =>
+    d.message.endsWith("borrow later used here")
+  )[0];
+  if (lateruseObj === undefined) {
+    return "cannot parse diagnostics";
+  }
+  // get the name of the borrower
+  if (lateruseObj.location.range.end.line !== lateruseObj.location.range.start.line) {
+    log.warn("user crossed multiple lines");
+  }
+  const lateruse = lateruseObj.location.range.end.line;
+  const line = editor.document.lineAt(lateruse).text;
+  const userfrom = lateruseObj.location.range.start.character;
+  const userto = lateruseObj.location.range.end.character;
+  const user = line.slice(userfrom, userto);
+  if (borrowed === undefined || validfrom === undefined || validto === undefined) {
+    return "cannot parse diagnostics";
+  }
+  const xshift = getXshift(editor, validfrom, lateruse) * G.charwidth;
+  return regionPointConflict(
+    xshift,
+    validfrom,
+    validto,
+    lateruse,
+    lateruse + 1,
+    `\`${user}\` borrows from \`${borrowed}\` and can be used in this region`,
+    `\`${borrowed}\` is no longer valid, while \`${user}\` is still borrowing it`,
+    `tip: make sure \`${user}\` borrows from a valid value`
+  );
 }
