c91e284bd1ea49c3caa431600e99bcb55c66f5b3
Wang Ruochen <wrc@ruo-chen.wang>
2023-05-02 15:33:03 -0700

    Bump

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import { log } from "./util";
 import { codeFuncMap } from "./visualizations";
 import * as fs from "fs";
 
-const VERSION = "0.0.6";
+const VERSION = "0.0.7";
 let intervalHandle: number | null = null;
 export function activate(context: vscode.ExtensionContext) {
   if (!vscode.workspace.workspaceFolders) {
