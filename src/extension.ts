83b64b8f3814762f80a2be933d5fd53271b438e2
Wang Ruochen <wrc@ruo-chen.wang>
2023-05-16 20:38:08 -0700

    Add icon

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import { log } from "./util";
 import { codeFuncMap } from "./visualizations";
 import * as fs from "fs";
 
-const VERSION = "0.1.0";
+const VERSION = "0.1.1";
 let intervalHandle: number | null = null;
 export function activate(context: vscode.ExtensionContext) {
   if (!vscode.workspace.workspaceFolders) {
